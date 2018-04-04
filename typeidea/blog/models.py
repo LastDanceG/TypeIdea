d1f9fb2f2aea37f1cd886a204eea72d08764e80a
lbs <ail>
2018-04-04 11:22:55 +0800

    完成了文章、分类、标签models的建立

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -1,6 +1,63 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, "正常"),
+        (STATUS_DELETE, "删除"),
+    )
+
+    name = models.CharField(max_length=50, verbose_name="名称")
+    status = models.IntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
+    owner = models.ForeignKey(User, verbose_name="作者")
+    is_nav = models.BooleanField(default=False, verbose_name="是否为导航")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = verbose_name_plural = "分类"
+
+
+class Tag(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, "正常"),
+        (STATUS_DELETE, "删除"),
+    )
+
+    name = models.CharField(max_length=50, verbose_name="标签")
+    status = models.IntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
+    owner = models.CharField(User, verbose_name="作者")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = verbose_name_plural = "标签"
+
+
+class Post(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_DRAFT = 2
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, "正常"),
+        (STATUS_DELETE, "删除"),
+        (STATUS_DRAFT, "草稿"),
+    )
+
+    title = models.CharField(max_length=255, verbose_name='标题')
+    owner = models.ForeignKey(User, verbose_name="作者")
+    category = models.ForeignKey(Category, verbose_name="分类")
+    tag = models.ManyToManyField(Tag, verbose_name="标签")
+    desc = models.CharField(max_length=1024, verbose_name="摘要")
+    content = models.TextField(verbose_name="正文", help_text="正文必须为MarkDown格式")
+    status = models.IntegerField(choices=STATUS_ITEMS, default=STATUS_NORMAL, verbose_name="状态")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = verbose_name_plural = "文章"
